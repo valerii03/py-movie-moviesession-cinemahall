2110905c1293276186ef8c37705f8d6f6a207ebf
Valerii <valeraivasik602@gmail.com>
2026-03-22 13:08:23 +0000

    Solution

---
+++ services/movie.py
@@ -0,0 +1,20 @@
+from db.models import Movie
+
+def get_movies(genres_ids=None, actors_ids=None):
+    qs = Movie.objects.all()
+    if genres_ids:
+        qs = qs.filter(genres__id__in=genres_ids)
+    if actors_ids:
+        qs = qs.filter(actors__id__in=actors_ids)
+    return qs.distinct()
+
+def get_movie_by_id(movie_id):
+    return Movie.objects.get(id=movie_id)
+
+def create_movie(movie_title, movie_description, genres_ids=None, actors_ids=None):
+    movie = Movie.objects.create(title=movie_title, description=movie_description)
+    if genres_ids:
+        movie.genres.set(genres_ids)
+    if actors_ids:
+        movie.actors.set(actors_ids)
+    return movie
---
+++ services/movie_session.py
@@ -0,0 +1,29 @@
+from db.models import MovieSession, Movie, CinemaHall
+
+def create_movie_session(movie_show_time, movie_id, cinema_hall_id):
+    movie = Movie.objects.get(id=movie_id)
+    cinema_hall = CinemaHall.objects.get(id=cinema_hall_id)
+    return MovieSession.objects.create(show_time=movie_show_time, movie=movie, cinema_hall=cinema_hall)
+
+def get_movies_sessions(session_date=None):
+    qs = MovieSession.objects.all()
+    if session_date:
+        qs = qs.filter(show_time__date=session_date)
+    return qs
+
+def get_movie_session_by_id(movie_session_id):
+    return MovieSession.objects.get(id=movie_session_id)
+
+def update_movie_session(session_id, show_time=None, movie_id=None, cinema_hall_id=None):
+    session = MovieSession.objects.get(id=session_id)
+    if show_time:
+        session.show_time = show_time
+    if movie_id:
+        session.movie = Movie.objects.get(id=movie_id)
+    if cinema_hall_id:
+        session.cinema_hall = CinemaHall.objects.get(id=cinema_hall_id)
+    session.save()
+    return session
+
+def delete_movie_session_by_id(session_id):
+    MovieSession.objects.filter(id=session_id).delete()
